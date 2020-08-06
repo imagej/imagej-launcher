30a7aea904cd506d15ad2803ef27baf2b997d63f
Mark Hiner <hinerm@gmail.com>
2020-08-06 10:28:02 -0500

    check-java-version: ensure imagej jar exists

--- check-java-version.sh
+++ check-java-version.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+if [ ! -f ~/.m2/repository/net/imagej/ij/1.53c/ij-1.53c.jar ];
+then
+  mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=net.imagej:ij:1.53c
+fi
+
 mkdir -p target/jars &&
-cp ~/.m2/repository/net/imagej/ij/1.53b/ij-1.53b.jar target/jars &&
+cp ~/.m2/repository/net/imagej/ij/1.53c/ij-1.53c.jar target/jars &&
 echo ' public class WhatJavaVersion { public static void main(String[] args) { System.out.println(System.getProperty("java.version")); } }' > WhatJavaVersion.java && 
 javac WhatJavaVersion.java &&
 
