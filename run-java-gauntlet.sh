4d7af98f566fb6b8f24f46774fb558c80a6e5a0c
Mark Hiner <hinerm@gmail.com>
2020-08-06 09:22:28 -0500

    Update run-java-gauntlet for platform dirs
    Bundled javas are now located in the /java/[platform]/ directory. The
    run-java-gauntlet script now accounts for this.

--- run-java-gauntlet.sh
+++ run-java-gauntlet.sh
@@ -3,7 +3,7 @@
 # run-java-gauntlet - A script to test the ImageJ native launcher against one
 # or more Java installation(s)
 # Inputs:
-#   - base directory containing Java installations (default: ~/Java_all/*/)
+#   - base directory containing a platform-specific folder ('macosx', 'win32', 'win64', 'linux32', 'linux64') with Java installations (default: ~/Java_all/*/)
 # Outputs:
 #   Summary logs for each Java installation, one file each, in
 #   "target/gauntlet_out"
@@ -14,49 +14,53 @@ else
   searchdir=$1
 fi
 
-# Ensure the native launcher is built
+# Ensure the native launcher is built and previous tests cleared
 mvn clean package
-
-# Make JRE discovery directory
-mkdir target/java
 
 # Make output dir
 mkdir target/gauntlet_out
 
-echo "Testing all Java installations in directory $searchdir"
+for pdir in $searchdir ; do
 
-for d in $searchdir ; do
-  echo "testing $d"
+	echo "Testing all Java installations in platform directory: $pdir"
 
-  expected=$( basename "$d" )
-  logfile=target/gauntlet_out/$expected.log
+	# Make a directory for this platform
+	platformname=$( basename "$pdir" )
+	mkdir -p "target/java/$platformname"
 
-  # Point imagej to this JRE
-  ln -sn $d target/java/$expected
+	# Test each java installation for this platform
+	for java in $(ls $pdir) ; do
+		javadir=$pdir$java
+		echo "testing $javadir"
 
-  # Run the script to check this JRE
-  source ./check-java-version.sh 2> $logfile
+		expected=$( basename "$javadir" )
+		logfile=target/gauntlet_out/$expected.log
 
-  # Extract the actual JRE used
-  actual=$( echo $( tail -n 1 $logfile )|cut -d '=' -f2 )
-  actual=$(basename "$actual")
+		# Point imagej to this JRE
+		echo "linking $javadir to target/java/$platformname/$expected"
+		ln -sn $javadir "target/java/$platformname/$expected"
 
-  # Test if the correct JRE was used
-  echo "actual: $actual" >> $logfile
-  echo "expected: $expected" >> $logfile
-  success="FAILED"
-  if [ "$actual" == "$expected" ]; then
-    success="PASSED"
-  fi
-  echo "$success" >> $logfile
+		# Run the script to check this JRE
+		source ./check-java-version.sh 2> $logfile
 
-  # Mark the file as pased/failed
-  mv $logfile target/gauntlet_out/$success.$expected.log
+		# Extract the actual JRE used
+		actual=$( echo $( tail -n 1 $logfile )|cut -d '=' -f2 )
+		actual=$(basename "$actual")
 
-  # NB: this is ESSENTIAL because the adjust_java_home_if_necessary() method
-  # does a two-pass search such that, even if a desired JDK is "newer", if it
-  # is found by the second pass, then even an "older" JDK found by the first
-  # pass will win.
-  # Rename this JRE out of the "java" folder so it is not discovered in future tests
-  mv target/java/$expected target/$expected
+		# Test if the correct JRE was used
+		echo "actual: $actual" >> $logfile
+		echo "expected: $expected" >> $logfile
+		success="FAILED"
+		if [ "$actual" == "$expected" ]; then
+	  		success="PASSED"
+		fi
+		echo "$success" >> $logfile
+
+		# Mark the file as pased/failed
+		mv $logfile target/gauntlet_out/$success.$expected.log
+
+		# NB: we currently don't guarantee behavior if multiple javas are present.
+		# Rename this JRE out of the "java" folder so it is not discovered in future tests
+		mv target/java/$platformname/$expected target/$expected
+	done
 done
