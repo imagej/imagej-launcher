e0f93b921e87b3b34a424fe9ad339b02f0126061
Curtis Rueden <ctrueden@wisc.edu>
2020-08-06 12:11:04 -0500

    check-java-version: tidy up a little bit

--- check-java-version.sh
+++ check-java-version.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ ! -f ~/.m2/repository/net/imagej/ij/1.53c/ij-1.53c.jar ];
-then
-  mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=net.imagej:ij:1.53c
-fi
+set -e
 
-mkdir -p target/jars &&
-cp ~/.m2/repository/net/imagej/ij/1.53c/ij-1.53c.jar target/jars &&
-echo ' public class WhatJavaVersion { public static void main(String[] args) { System.out.println(System.getProperty("java.version")); } }' > WhatJavaVersion.java && 
-javac WhatJavaVersion.java &&
+ijJAR="$HOME/.m2/repository/net/imagej/ij/1.53c/ij-1.53c.jar"
+test -f "$ijJAR" || mvn dependency:get -Dartifact=net.imagej:ij:1.53c
+
+mkdir -p target/jars
+cp "$ijJAR" target/jars
+echo ' public class WhatJavaVersion { public static void main(String[] args) { System.out.println(System.getProperty("java.version")); } }' > WhatJavaVersion.java
+javac WhatJavaVersion.java
 
 DEBUG=1 $(find target/ -maxdepth 1 -name "ImageJ-*") --main-class WhatJavaVersion
