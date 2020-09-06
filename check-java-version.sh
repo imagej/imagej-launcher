c631ea916e69b1b4b0fcdca7ee4fcbb0ae49e9cd
Curtis Rueden <ctrueden@wisc.edu>
2020-09-06 06:57:53 -0500

    check-java-version.sh: emit more details
    This tells us exactly which java was used.

--- check-java-version.sh
+++ check-java-version.sh
@@ -7,7 +7,15 @@ test -f "$ijJAR" || mvn dependency:get -Dartifact=net.imagej:ij:1.53c
 
 mkdir -p target/jars
 cp "$ijJAR" target/jars
-echo ' public class WhatJavaVersion { public static void main(String[] args) { System.out.println(System.getProperty("java.version")); } }' > WhatJavaVersion.java
-javac WhatJavaVersion.java
+echo '
+public class WhichJava {
+  public static void main(String[] args) {
+    String[] props = {"java.home", "java.vendor", "java.version"};
+    for (String p : props)
+      System.out.println(p + " = " + System.getProperty(p));
+  }
+}
+' > WhichJava.java
+javac WhichJava.java
 
-DEBUG=1 $(find target/ -maxdepth 1 -name "ImageJ-*") --main-class WhatJavaVersion
+DEBUG=1 $(find target/ -maxdepth 1 -name "ImageJ-*") --main-class WhichJava
