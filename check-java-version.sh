169a7d5d9dbcff14a00bcaf033ee73eb66ba113e
Mark Hiner <hinerm@gmail.com>
2020-11-10 10:20:31 -0600

    check-java-version: improve subscript behavior
    When check-java-version is called from another script
    (run-java-gauntlet) it runs in a subshell, which collects std
    output/error from imagej but not from Java. Thus the actual output of
    the WhichJava program is lost to the run-java-gauntlet program and thus
    cannot be used to verify the pass/fail status of the test.
    
    By instead writing the output to a temporary file and catting the
    result, the check-java-version script essentially "produces" the Java
    program's output to stdout, while maintaining stderr from the native
    launcher (which is desired to preserve debug output from the launcher).
    
    As the stderr and stdout are now both present, note that the order of
    WhichJava output has been adjusted so that the actual Java used appears
    on the last line (to simplify pass/fail testing).

--- check-java-version.sh
+++ check-java-version.sh
@@ -8,9 +8,12 @@ test -f "$ijJAR" || mvn dependency:get -Dartifact=net.imagej:ij:1.53c
 mkdir -p target/jars
 cp "$ijJAR" target/jars
 echo '
+import java.io.*;
+
 public class WhichJava {
-  public static void main(String[] args) {
-    String[] props = {"java.home", "java.vendor", "java.version"};
+  public static void main(String[] args) throws FileNotFoundException {
+    String[] props = {"java.version", "java.vendor", "java.home"};
+    System.setOut(new PrintStream(new File("which_java.out")));
     for (String p : props)
       System.out.println(p + " = " + System.getProperty(p));
   }
@@ -18,4 +21,7 @@ public class WhichJava {
 ' > WhichJava.java
 javac WhichJava.java
 
-DEBUG=1 $(find target/ -maxdepth 1 -name "ImageJ-*") --main-class WhichJava
+DEBUG=1 $(find target/ -maxdepth 1 -name "ImageJ-*") --attach-console --main-class WhichJava
+
+cat which_java.out
+rm which_java.out
